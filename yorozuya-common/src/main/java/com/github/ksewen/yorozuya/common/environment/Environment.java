555479b0d3f6519afa108781cbde90a137f1fd5f
ksewen <261517946@qq.com>
2023-08-25 18:22:38 +0300

    add a unified API for retrieving system environment information with its implementation and testing. config jacoco maven plugin to display the coverage.

---
+++ yorozuya-common/src/main/java/com/github/ksewen/yorozuya/common/environment/Environment.java
@@ -0,0 +1,21 @@
+package com.github.ksewen.yorozuya.common.environment;
+
+import java.util.Map;
+
+/**
+ * @author ksewen
+ * @date 25.08.2023 15:24
+ */
+public interface Environment {
+
+    String getHostName();
+
+    String getHostIp();
+
+    String getEnvironment();
+
+    String getApplicationName();
+
+    Map<String, String> getMetadata();
+
+}
---
+++ yorozuya-common/src/test/java/com/github/ksewen/yorozuya/common/environment/impl/BasicEnvironmentTest.java
@@ -0,0 +1,52 @@
+package com.github.ksewen.yorozuya.common.environment.impl;
+
+import com.github.ksewen.yorozuya.common.constant.SystemConstants;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author ksewen
+ * @date 25.08.2023 16:53
+ */
+class BasicEnvironmentTest {
+
+    private BasicEnvironment environment = BasicEnvironment.getInstance();
+
+    @Test
+    void getHostName() {
+        assertThat(this.environment.getHostName()).isNotEmpty();
+    }
+
+    @Test
+    void getHostIp() {
+        assertThat(this.environment.getHostIp()).isNotEmpty();
+    }
+
+    @Test
+    void getEnvironment() {
+        String env = "test";
+        assertThat(this.environment.getEnvironment()).isEqualTo(SystemConstants.PROPERTIES_NOT_SET_VALUE);
+        this.environment.initEnvironment(env);
+        assertThat(this.environment.getEnvironment()).isEqualTo(env);
+        this.environment.initEnvironment(env + "1");
+        assertThat(this.environment.getEnvironment()).isEqualTo(env);
+    }
+
+    @Test
+    void getApplicationName() {
+        String app = "yorozuya";
+        assertThat(this.environment.getApplicationName()).isEqualTo(SystemConstants.PROPERTIES_NOT_SET_VALUE);
+        this.environment.initApplicationName(app);
+        assertThat(this.environment.getApplicationName()).isEqualTo(app);
+        this.environment.initApplicationName(app + "1");
+        assertThat(this.environment.getApplicationName()).isEqualTo(app);
+    }
+
+    @Test
+    void getMetadata() {
+        String key = "test-key";
+        String value = "test-value";
+        assertThat(this.environment.getMetadata()).isNotNull();
+    }
+}
