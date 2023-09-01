d4b5415fed29a4af8c0fa7f6504de01396b694c8
ksewen <261517946@qq.com>
2023-09-01 17:22:00 +0300

    adding the yorozuya-test module and configuring the Jacoco plugin to collecting coverage reports update README.md to add building badge correcting the project naming error

---
+++ yorozuya-samples/rest-client-okhttp/src/main/java/com/github/ksewen/yorozuya/sample/rest/client/okhttp/RestClientOkhttpApplication.java
@@ -0,0 +1,12 @@
+package com.github.ksewen.yorozuya.sample.rest.client.okhttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientOkhttpApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RestClientOkhttpApplication.class, args);
+  }
+}
