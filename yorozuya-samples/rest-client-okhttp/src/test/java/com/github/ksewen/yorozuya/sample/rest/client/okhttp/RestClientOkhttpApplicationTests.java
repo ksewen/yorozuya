dda83faff86cbc72df0545e66c1240175cc3f350
ksewen <261517946@qq.com>
2023-09-21 17:35:48 +0300

    integrate with spring-boot-data-jpa-starter add a sample for jpa usage and testing update README.md to add documentation for this feature

--- yorozuya-samples/rest-client-okhttp/src/test/java/com/github/ksewen/yorozuya/sample/rest/client/okhttp/RestClientOkhttpApplicationTests.java
+++ yorozuya-samples/rest-client-okhttp/src/test/java/com/github/ksewen/yorozuya/sample/rest/client/okhttp/RestClientOkhttpApplicationTests.java
@@ -1,9 +1,14 @@
 package com.github.ksewen.yorozuya.sample.rest.client.okhttp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(
+    exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 class RestClientOkhttpApplicationTests {
 
   @Test
