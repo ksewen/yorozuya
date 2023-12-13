cdda933cbb21364333a55d2c936cb31793b37a60
ksewen <261517946@qq.com>
2023-12-13 18:30:59 +0300

    try to fix the test failure in the class OpenAPIBeanTest during the ci

--- yorozuya-spring-boot-starter/src/test/java/com/github/ksewen/yorozuya/starter/configuration/documentation/OpenAPIBeanTest.java
+++ yorozuya-spring-boot-starter/src/test/java/com/github/ksewen/yorozuya/starter/configuration/documentation/OpenAPIBeanTest.java
@@ -1,14 +1,18 @@
 package com.github.ksewen.yorozuya.starter.configuration.documentation;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import com.github.ksewen.yorozuya.common.constant.SystemConstants;
-import com.github.ksewen.yorozuya.starter.configuration.environment.EnvironmentAutoConfiguration;
+import com.github.ksewen.yorozuya.common.environment.Environment;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author ksewen
@@ -16,7 +20,10 @@ import org.springframework.boot.test.context.SpringBootTest;
  */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(
-    classes = {SpringDocAutoConfiguration.class, EnvironmentAutoConfiguration.class},
+    classes = {
+      SpringDocAutoConfiguration.class,
+      OpenAPIBeanTest.MockEnvironmentAutoConfiguration.class
+    },
     properties = {"springdoc.version=3.0.0", "springdoc.license.url=https://www.test.com"})
 public class OpenAPIBeanTest {
 
@@ -24,6 +31,7 @@ public class OpenAPIBeanTest {
 
   @Test
   void test() {
+
     assertThat(openAPI)
         .matches(o -> o.getInfo() != null, "info object is null")
         .matches(
@@ -41,5 +49,16 @@ public class OpenAPIBeanTest {
         .matches(
             o -> "https://www.test.com".equals(o.getInfo().getLicense().getUrl()),
             "license url is not expected");
+  }
+
+  @Configuration
+  static class MockEnvironmentAutoConfiguration {
+
+    @Bean
+    public Environment mockEnvironment() {
+      Environment environment = mock(Environment.class);
+      when(environment.getApplicationName()).thenReturn(SystemConstants.PROPERTIES_NOT_SET_VALUE);
+      return environment;
+    }
   }
 }
